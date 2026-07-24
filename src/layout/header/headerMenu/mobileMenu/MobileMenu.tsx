436ed6cd6fbcffc44e14fd231c8b95d5e28e6770
dimadubi <dimadubi666@gmail.com>
2026-07-24 19:32:01 +0300

    feat: add toggle functionality to burger menu

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -1,18 +1,23 @@
 import { S } from "../HeaderMenu_Styles.ts";
 import {Menu} from "../menu/Menu.tsx";
 import * as React from "react";
+import {useState} from "react";
 
 
 
 export const MobileMenu: React.FC<{menuItems: Array<string>}> = (props: {menuItems: Array<string>}) => {
+
+    const [menuIsOpen, setmenuIsOpen] = useState(false)
+    const onBurgerBtnClick = () => {setmenuIsOpen(!menuIsOpen)}
+
     return (
         <S.MobileMenu>
 
-            <S.BurgerButton isOpen = {false}>
+            <S.BurgerButton isOpen = {menuIsOpen} onClick={onBurgerBtnClick}>
                 <span></span>
             </S.BurgerButton>
 
-            <S.MobileMenuPopup isOpen = {false}>
+            <S.MobileMenuPopup isOpen = {menuIsOpen} onClick={()=>{setmenuIsOpen(false)}}>
                 <Menu menuItems={props.menuItems}/>
             </S.MobileMenuPopup>
 
